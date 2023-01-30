e10527517ada7491019a6ba6cd42266e4cc2452c
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2023-01-29 22:36:30 -0800

    Create default_graphics.java
    Create New Class with PseudoCode

---
+++ src/main/java/classes/default_graphics.java
@@ -0,0 +1,27 @@
+package classes;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
+import javax.swing.Timer;
+
+import acm.graphics.GImage;
+import acm.graphics.GLabel;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+import edu.pacific.comp55.starter.GraphicsApplication;
+
+public class default_graphics extends Map implements KeyListener {
+	// Create New Class with Code
+	// Copy down functions and output wanted types
+	// Remove Extra Code
+	
+	public static void main(String args[]) {
+		
+	}
+
+}
